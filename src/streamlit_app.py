55000a06c73d337349e0737ca0e56efcf1594530
Maanvi Chawla <mc823@duke.edu>
2025-12-08 11:21:58 -0500

    local disco

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -37,7 +37,7 @@ def get_base64_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
 
-disco_ball_base64 = get_base64_image("data/disco.png")
+disco_ball_base64 = get_base64_image("src/disco.png")
 
 if 'page' not in st.session_state:
     st.session_state.page = 'search'
